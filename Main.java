9fd88cc774cdaf300c58417ab52d4f2460c39fe5
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-01-07 22:19:53 -0600

    Inicialización de repositorio, agregamos la clase principal para inicializacion de estrucutras

---
+++ Main.java
@@ -0,0 +1,69 @@
+/**
+ *Principal class to initialize structs
+ * Test your code in this class
+ */
+
+import structs.*;
+
+public class Main{
+
+    //Callback
+    public static void main(){
+        
+        //Implementation and test!
+        //in this part only call test_methods
+        
+        testQueue();//Add parameters if you need any
+        
+        testStack();//Add parameters if you need any
+         
+        testSimple_LinkedList();//Add parameters if you need any
+         
+        testDouble_LinkedList();//Add parameters if you need any
+         
+        testParallelArray();//Add parameters if you need any
+         
+        testBinaryTree();//Add parameters if you need any  
+        
+    }
+    
+    /**
+     * Struct : Queue
+     * put your code here to test this struct
+     */
+     public static void testQueue(){}
+     
+     /**
+     * Struct : Stack
+     * put your code here to test this struct
+     */
+     public static void testStack(){}
+     
+     /**
+     * Struct : Simple Linked List
+     * put your code here to test this struct
+     */
+     public static void testSimple_LinkedList(){}
+     
+     /**
+     * Struct : Doble linked list
+     * put your code here to test this struct
+     */
+     
+     public static void testDouble_LinkedList(){}
+     
+     /**
+     * Struct : Parallel Array
+     * put your code here to test this struct
+     */
+    
+    public static void testParallelArray(){}
+    
+    /**
+     * Struct : Binary Tree
+     * put your code here to test this struct
+     */
+     
+     public static void testBinaryTree(){}
+         
+}
