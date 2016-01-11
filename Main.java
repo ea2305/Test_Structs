4be1636ca04fd7f6cfcd29bb3c3abd3de2506bed
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-01-11 11:29:42 -0600

    Cambios al menu principal

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@
  */
 
 import structs.double_list.*;//Uncoment after put one struct in package
+import java.util.Scanner;
 
 public class Main{
 
@@ -20,17 +21,7 @@ public class Main{
         //Implementation and test!  
         //in this part only call test_methods
         
-        testQueue();//Add parameters if you need any
-        
-        testStack();//Add parameters if you need any
-         
-        testSimple_LinkedList();//Add parameters if you need any
-         
-        testDouble_LinkedList();//Add parameters if you need any
-         
-        testParallelArray();//Add parameters if you need any
-         
-        testBinaryTree();//Add parameters if you need any  
+        menu(input);
         
     }
     
@@ -59,20 +50,47 @@ public class Main{
      
      public static void testDouble_LinkedList(String[] input){
          
-         //Implementation of struct
-         Doble_LinkedList<String> Linked_List  = new Doble_LinkedList<>();
-         
-         System.out.println("\n***Pruebas para Lista doblemente enlazada***\n");
-         
-         System.out.println("\tContenido sin datos, Incial : " + Linked_List.size() + "\n");
-         
-         System.out.println(">> Carga de Elemetos a la lista enlazada\n");
-         for(String elements: input){
-             System.out.print("[" + elements + "]<=>");
-             Linked_List.add(elements);
-         }
-         
-         System.out.println("\n\n\tContenido con datos cargados, Final: " + Linked_List.size());;
+        //Implementation of struct
+        Doble_LinkedList<String> Linked_List  = new Doble_LinkedList<>();
+        
+        //add test
+        System.out.println("\n\t***[ PRUEBAS DE LISTA DOBLEMENTE ENLAZADA ]***\n");
+
+        System.out.println("\tContenido sin datos, Incial : " + Linked_List.size() + "\n");
+
+        //Load elements in linked list
+        System.out.println(">> Carga de Elemetos a la lista enlazada\n");
+        for(String elements: input){
+            System.out.print("[" + elements + "]<=>");
+            Linked_List.add(elements);
+        }
+
+        System.out.println("\n\n\tContenido con datos cargados, Final: " + Linked_List.size());;
+
+        //Check elements until last element
+        System.out.println("\n\n***Recorrido de datos de manera ascendente, del primer nodo al ultimo");
+        Linked_List.listUp();
+        
+        //Check elements until last element
+        System.out.println("\n\n***Recorrido de datos de manera descendente, del ultimo nodo al primero");
+        Linked_List.listDown();
+        
+        //add in position
+        System.out.println("\n***Agregamos elemento en la segunda posicion");
+        Linked_List.addPos(2,"Elemento agregado <- en posicion 2");
+        Linked_List.listUp();
+        
+        //remove in position
+        System.out.println("\n***Removemos elemento en la segunda posicion");
+        Linked_List.remove(2);
+        Linked_List.listUp();
+        
+        //Search test
+        System.out.println("\nPrueba de busqueda de elementos en la lista");
+        System.out.print("\n>Busqueda del 8vo elemento  : " + Linked_List.get(8).getData());
+        System.out.print("\n>Busqueda del 10mo elemento : " + Linked_List.get(10).getData());
+        System.out.print("\n>Busqueda del 3vo elemento  : " + Linked_List.get(3).getData() + "\n\n");
+        
      }
      
      /**
@@ -86,7 +104,57 @@ public class Main{
      * Struct : Binary Tree
      * put your code here to test this struct
      */
-     
      public static void testBinaryTree(){}
+     
+     public static void menu(String[] input){
+         
+         //strucst for options
+         int select = 0;         
+         Scanner read = new Scanner(System.in);
+         String [] options = {"1.-Cola","2.-Pila","3.-Lista Simple",
+                              "4.-Lista Doblemente Enlazada","5.-Arreglo Paralelo",
+                              "6.-Arbol Binario","[7...10).-Salir"};
+                              
+         while(select != -1){//Close when option is breaked
+         
+            select = 0;  
+            System.out.println("\n\t*************[ MENU PRINCIPAL ]****************");
+            
+            for(String element:options)
+                System.out.println(element);
+                
+            try{
+                select = read.nextInt();//read option
+                switch(select){
+                    case 1:
+                        testQueue();
+                        break;
+                    case 2:
+                        testStack();
+                        break;
+                    case 3:
+                        testSimple_LinkedList();
+                        break;
+                    case 4:
+                        testDouble_LinkedList(input);
+                        break;
+                    case 5:
+                        testParallelArray();
+                        break;
+                    case 6:
+                        testBinaryTree();
+                        break;
+                    default:
+                        select = -1;
+                        break;
+                }
+            }catch(Exception e){//if input isn't integer 
+                System.out.println("\n\tImportante! -> Introduzca solo numeros\n");
+                read = new Scanner(System.in);//restart reader
+            }
+         
+         } 
+         
+     }
          
 }
--- structs/double_list/Doble_LinkedList.java
+++ structs/double_list/Doble_LinkedList.java
@@ -6,8 +6,6 @@
  */
 package structs.double_list;
 
-//import structs.double_list.*;
-
 
 public class Doble_LinkedList<T extends Comparable<T>>{
 
@@ -22,6 +20,7 @@ public class Doble_LinkedList<T extends Comparable<T>>{
         
         head = new Node<T>(null);
         foot = new Node<T>(null);
+        
         head.setNext(foot);//Asignamos como siguiente elemento
         foot.setPrev(head);//Creamos elemento anterior
 
@@ -45,14 +44,13 @@ public class Doble_LinkedList<T extends Comparable<T>>{
         // Se tiene que posicionar en la cabeza para poder
         //recorrer la lista y llegar al fin
 
-        while (Current.getNext().getNext() != null) {
+        while (Current.getNext().getNext() != null) 
             Current = Current.getNext();
-        }
 
         Temp.setPrev(Current);// Asignamos el nuevo nodo como "next"
         Temp.setNext(foot);//Cambiamos le pie de busqueda !!
-        Current.setNext(Temp);
-        foot.setPrev(Temp);
+        Current.setNext(Temp);//Apuntamos el penultimo elemento al ultimo elemento
+        foot.setPrev(Temp);//Apuntamos el footer al ultimo elemento
 
         listCount++;// incrementamos el contador de nodos
     }
@@ -63,20 +61,13 @@ public class Doble_LinkedList<T extends Comparable<T>>{
 
         if (index <= 0 || index > this.listCount)
             return null;
-
-
-
-        //if(index == 1)
-          Current = head.getNext();
-
-        //else
-          //Current = head.getNext().getNext();
+            
+        Current = head.getNext();
 
         for (int i = 1; i < index; i++) {
-//Provisional
             if (Current.getNext() == null)
                 return null;
-             Current = Current.getNext();
+            Current = Current.getNext();
         }
         return Current;
     }
@@ -90,7 +81,6 @@ public class Doble_LinkedList<T extends Comparable<T>>{
         Node<T> Current = foot.getPrev();
 
         for (int i = size(); i < index; i--) {
-
             if (Current.getPrev() == null)
                 return null;
              Current = Current.getPrev();
@@ -101,7 +91,8 @@ public class Doble_LinkedList<T extends Comparable<T>>{
 
     // remueve el elemento en la posición indicada
     public boolean remove(int index){
-        //
+        
+        //Verificamos los rangos de busqueda
         if (index < 1 || index > size())
             return false;
 
@@ -112,7 +103,10 @@ public class Doble_LinkedList<T extends Comparable<T>>{
                 return false;
              Current = Current.getNext();
         }
+        //Apuntamos a el siguiente del siguiente elemento        
         Current.setNext(Current.getNext().getNext());
+        
+        //Evitamos modificacion del head
         if(index != 1)
           Current.setPrev(Current.getPrev().getPrev());
 
@@ -160,6 +154,32 @@ public class Doble_LinkedList<T extends Comparable<T>>{
             a++;
         }
         return addPos(a,data);
+    }
+    
+    public void listUp(){
+        
+        int index = 1;
+        //Apuntamos al primer elemento de la lista
+        Node<T> Current = head.getNext();
+        
+        while (Current != null) {
+            System.out.println("\t[" + index + " : " + Current.getData() + "]");
+            Current =  Current.getNext();
+            index++;
+        }
+    }
+    
+    public void listDown(){
+        
+        int index = size();
+        //Apuntamos al primer elemento de la lista
+        Node<T> Current = foot.getPrev();
+        
+        while (Current != null) {
+            System.out.println("\t[" + index + " : " + Current.getData() + "]");
+            Current =  Current.getPrev();
+            index--;
+        }
     }
 
     // returns the number of elements in this list.
