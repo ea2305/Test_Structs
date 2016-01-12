5aeb05139286b08d4689caf482207552aa4abb71
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-01-12 14:59:16 -0600

    Agregamos clear fix para limpieza de las clases con >sh

---
+++ clearClass.sh
@@ -0,0 +1,7 @@
+#Posicionamos el puntero en el directorio del usuario
+#removemos todas las clases de la carpeta structs
+
+rm ./*.class ./structs/arbol/*.class ./structs/arregloParalelo/*.class ./structs/double_list/*.class
+
+echo "Script ejecutado correctamente, verifique los archivos .class eliminados"
+
