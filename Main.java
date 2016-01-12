0a606eb97dd24c99d5402eb21a9e40e589ea40bd
maxxescobarr <maxxescobarr@gmail.com>
2016-01-11 20:05:11 -0600

    fdasfda asfas

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@
 
 import structs.double_list.*;//Uncoment after put one struct in package
 import java.util.Scanner;
-//Probando GitHub
+//Probando GitHub desde Ubuntu
 public class Main{
 
     //Callback
